50f1e2b58df2ce5c2dd727858276d16492bd8dca
QinHai0627 <47602326+QinHai0627@users.noreply.github.com>
2020-09-27 19:49:01 +0800

    Update rmsprop.py

--- rmsprop.py
+++ rmsprop.py
@@ -60,13 +60,13 @@ def load_data(data_folder):
         test_labels = np.frombuffer(lbpath.read(),np.uint8,offset=8)
         
     with gzip.open(paths[3],'rb') as imgpath:
-        test_labels=np.frombuffer(imgpath.read(),np.uint8,offset=16).reshape(len(test_labels),28,28)
+        test_images=np.frombuffer(imgpath.read(),np.uint8,offset=16).reshape(len(test_labels),28,28)
 
     return (train_images,train_labels),(test_images,test_labels)
 
 (train_images,train_labels), (test_images,test_labels) = load_data('C:/Users/QinHai/Downloads/Compressed')
 
-train_labels, test_labels = train_labels / 255.0, test_labels / 255.0
+#train_labels, test_labels = train_labels / 255.0, test_labels / 255.0
 
 model=tf.keras.models.Sequential([
     tf.keras.layers.Dense(812,activation='relu',input_shape=(28*28,)),
@@ -77,13 +77,13 @@ model=tf.keras.models.Sequential([
 model.compile(optimizer='rmsprop',loss='categorical_crossentropy',metrics=['accuracy'])
 
 train_images = train_images.reshape((60000,28*28))
-train_images = train_images.astype('float32')/255
+#train_images = train_images.astype('float32')/255
  
 test_images = test_images.reshape((10000,28*28))
-test_images = test_images.astype('float32')/255
+#test_images = test_images.astype('float32')/255
 
 train_labels = keras.utils.to_categorical(train_labels)
 test_labels = keras.utils.to_categorical(test_labels)
-model.fit(train_images,train_labels,epochs=5,batch_size=128)
+model.fit(train_images,train_labels,epochs=5,batch_size=16)
 test_loss,test_acc = model.evaluate(test_images,test_labels)
 print('test_acc:',test_acc)
